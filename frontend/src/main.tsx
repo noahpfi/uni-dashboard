c8aed53c46e7e9b5167952c38dd7a667217759f2
Noah <noahpfst@gmail.com>
2026-06-13 13:16:45 +0200

    iframe embed theme sync

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (window !== window.top) document.documentElement.classList.add('in-iframe')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
